e1174724819cc4fa7674b0dd70a98cea16a365fc
hawk.0fcx <67156465+hawk-unity@users.noreply.github.com>
2021-07-11 10:42:49 +0300

    Update p.py

--- p.py
+++ p.py
@@ -1,104 +1,32 @@
-import requests
-import socket
 import os
-import whois
-import urllib.request
-from ipdata import ipdata
-from pprint import pprint
+import tkinter as tk
 import sys
 import time
-import colorama
-from colorama import Fore, Back, Style, init
+import json
+import subprocess
+from requests import get
 import sys
-import hashlib
-os.system("clear")
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(5)
-print(Fore.RED + """
----------------------------------------------
-      1.PORT TARAMA
----------------------------------------------
-      2.SAYFA KAYNAK KODU ÇEKME
----------------------------------------------
-      3.İP TRACER
----------------------------------------------
-      4.WHOİS SORGULAMA
----------------------------------------------
-      5.SİTE İP ADRESİ GÖRÜNTÜLE
----------------------------------------------
-      6.SHA512 ŞİFRELEME
----------------------------------------------
-      7.MD5 ŞİFRELEME
----------------------------------------------
-      8.SQLMAP VERİTABANI ÇEKME
----------------------------------------------
-      9.Bilgi toplama
----------------------------------------------
-      """)
-veri = input(Fore.GREEN + "Islem Numarasini Girin: ")
-if (veri == "1" ) :
-    host = input( "LÜTFEN İP ADRESİNİ GİRİNİZ : ")
-    port = int(input("TARATILACAK PORT ADRESİNİ GİRİNİZ : "))
-
-def portScanner(port):
-    if s.connect_ex((host, port)):
-        print(Fore.RED + "BU PORT KAPALI")
-    else:
-        print(Fore.RED + "BU PORT AÇIK")
-    portScanner(port)
-
-if (veri == "2" ) :
-
-    url = input(Fore.RED + "(example:https://www.google.com)URL : ")
-    r = requests.get(url)
-    txt = r.text
-    print(txt)
-
-if (veri == "3" ) :
-
-    hawk = input("-->>")
-    ipdata = ipdata.IPData('test')
-    response = ipdata.lookup(hawk)
-    pprint(response)
-
-if (veri == "4" ) :
-    domain = input("İP VEYA URL : ")
-    ss = whois.whois(domain)
-    print (ss)
-    print("işin bitti sie")
-
-if (veri == "5" ) :
-    dom = input("İP VEYA URL  : ")
-    print(socket.gethostbyname(dom))
-
-if (veri == "6" ) :
-    m=hashlib.sha512()
-    sha512 = input("STRİNG : ")
-    m.update(sha512.encode('utf-8'))
-    print(m.hexdigest())
-if (veri== "7" ) :
-    sifreleyici = hashlib.md5()
-    metin = input("lütfen Hashlenecek metini giriniz: ")
-    sifreleyici.update(metin.encode("utf-8"))
-    hash = sifreleyici.hexdigest()
-    print("Çıktı>>> %s" % hash)
-if (veri== "8" ) :
-     açık = input("Açığı olan site : ")
-     os.system("sqlmap -u"+açık+" --dbs --random-agent")
-if (veri== "9") :
-    d3 = input('Enter IP Or Domain +hawk bilgi toplama aracı+: ')
-    os.system(" clear ")
-    os.system("curl http://api.hackertarget.com/whois/?q=" + d3 )
-    time.sleep( 5 )
-    os.system(" && ")
-    os.system("curl http://api.hackertarget.com/dnslookup/?q=" + d3 )
-    time.sleep( 5 )
-    os.system(" && ")
-    os.system("curl https://api.hackertarget.com/reversedns/?q=" + d3 )
-    time.sleep( 5 )
-    os.system(" && ")
-    os.system("curl http://api.hackertarget.com/geoip/?q=" + d3 )
-    time.sleep( 5 )
-    os.system(" && ")
-    os.system("curl http://api.hackertarget.com/nmap/?q=" + d3 )
-    time.sleep( 5 )
+print("[+] ----------------HAWK-UNİTY----------------------[+]")
+print("[-] Author : HAWK UNİTY ")
+print("[-] GİT HUB : https://github.com/hawk-unity ")
+print("[-] İNSTAGRAM : https://instagram.com/4zusagawa ")
+print("[-] YOU TUBE : Bir Coder ")
+print("[+] ----------------HAWK-UNİTY----------------------[+]")
+print("[+] ----------------HAWK-UNİTY----------------------[+]")
+target= input("[+]Target -> : ")
+result = get('http://api.hackertarget.com/nmap/?q=' + target).text
+sys.stdout.write(result)
+result2 = get('http://api.hackertarget.com/dnslookup/?q=' + target).text
+sys.stdout.write(result2)
+result3 = get('http://api.hackertarget.com/geoip/?q=' + target).text
+sys.stdout.write(result3)
+i=tk.Tk()
+i.configure(background='black')
+i.title("MULTİ TOOL : H4WK OFCX")
+greeta = tk.Label(i,text=(result) ,font='Verdana 10', bg="red" , fg="black")
+greeta.grid(column=0, row=1)
+greeta2 = tk.Label(i,text=(result2) ,font='Verdana 10', bg="white",fg="red")
+greeta2.grid(column=0, row=2)
+greeta2 = tk.Label(i,text=(result3) ,font='Verdana 10', bg="white",fg="red")
+greeta2.grid(column=0, row=3)
+i.mainloop()
